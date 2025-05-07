4ef51d6a5d672375f8bd0040c33dcf03b5d06286
krish01110 <krishgaur0110@gmail.com>
2025-05-07 14:14:43 +0530

    commit

--- src/main/java/org/ncu/mf_loan_system/service/LoanServiceImpl.java
+++ src/main/java/org/ncu/mf_loan_system/service/LoanServiceImpl.java
@@ -1,9 +1,12 @@
 package org.ncu.mf_loan_system.service;
 
 import org.ncu.mf_loan_system.entities.Loan;
+import org.ncu.mf_loan_system.entities.Payment;
 import org.ncu.mf_loan_system.repository.LoanRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -50,5 +53,23 @@ public class LoanServiceImpl implements LoanService {
     public void deleteLoan(Long id) {
         Loan loan = getLoanById(id);
         loanRepository.delete(loan);
+    }
+
+
+    @Override
+    public BigDecimal getTotalOutstandingAmount() {
+        List<Loan> allLoans = loanRepository.findAll();
+        BigDecimal totalOutstanding = BigDecimal.ZERO;
+
+        for (Loan loan : allLoans) {
+            BigDecimal loanAmount = loan.getPrincipalAmount(); // assuming full amount is pending
+            BigDecimal paidAmount = loan.getPayments().stream()
+                    .map(Payment::getAmount)
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+            BigDecimal outstanding = loanAmount.subtract(paidAmount);
+            totalOutstanding = totalOutstanding.add(outstanding);
+        }
+
+        return totalOutstanding;
     }
 }
