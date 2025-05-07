d9b8d7da98bcfc11d73042203b8874c129a15537
Ronit <ronitbajaj05@gmail.com>
2025-05-08 03:06:37 +0530

    6th

--- src/main/java/org/ncu/mf_loan_system/LoggingAndAlertAspect.java
+++ src/main/java/org/ncu/mf_loan_system/LoggingAndAlertAspect.java
@@ -2,7 +2,7 @@ package org.ncu.mf_loan_system;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.ncu.mf_loan_system.service.LoanServiceImpl;
+//import org.ncu.mf_loan_system.service.LoanServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -15,18 +15,18 @@ public class LoggingAndAlertAspect {
     // Log when a loan is created
     @AfterReturning(value = "execution(* org.ncu.mf_loan_system.service.LoanService.createLoan(..))")
     public void logAfterLoanCreated(JoinPoint joinPoint) {
-        logger.info("New loan issued: " + joinPoint.getArgs()[0]);
+        logger.info("New loan issued: {}", joinPoint.getArgs()[0]);
     }
 
     // Log after a payment is recorded
     @AfterReturning(value = "execution(* org.ncu.mf_loan_system.service.PaymentService.createPayment(..))")
     public void logAfterPaymentCreated(JoinPoint joinPoint) {
-        logger.info("New payment recorded: " + joinPoint.getArgs()[0]);
+        logger.info("New payment recorded: {}", joinPoint.getArgs()[0]);
     }
 
     // Log errors
     @AfterThrowing(pointcut = "execution(* org.ncu.mf_loan_system.service..*(..))", throwing = "ex")
     public void logExceptions(JoinPoint joinPoint, Exception ex) {
-        logger.error("Exception in method: " + joinPoint.getSignature().getName(), ex);
+        logger.error("Exception in method: {}", joinPoint.getSignature().getName(), ex);
     }
 }
